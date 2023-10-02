d6e4cce834c1c46999779a5e8f4ee88d4a6378b0
yawkessey <yawkesseyankomah@gmail.com>
2023-10-02 16:45:10 -0400

    Vehicle class and Vehicle owner class

---
+++ CUS1166/src/project/User.java
@@ -0,0 +1,32 @@
+package project;
+
+public class User {
+	private String name;
+	private String password;
+	private String email;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return this.password();
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return this.email;	
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
