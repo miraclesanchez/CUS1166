845b48c4ad022806fca5eb72524cca3669550680
Dan.C <dcohen0110@gmail.com>
2023-10-03 08:33:20 -0400

    Added Two New Classes and More Adjustments
    Added the following:
    - Vehicle Owner View Class
    - Register Vehicle View Class
    - Added Listeners in GuiManager to accommodate the added classes
    - Adjusted test project to properly load gui components

--- CUS1166/src/project/TestProject.java
+++ CUS1166/src/project/TestProject.java
@@ -1,8 +1,18 @@
 package project;
 
+import java.awt.EventQueue;
+
 public class TestProject {
 
 	public static void main(String[] args) {
-		IntroGUI login = new IntroGUI();
+		EventQueue.invokeLater(new Runnable() {
+	        public void run() {
+//		IntroGUI login = new IntroGUI();
+//		RegisterVehicleView d = new RegisterVehicleView();
+		VehicleOwnerView d = new VehicleOwnerView();
+		
+	        }
+		});
 	}
 }
+	
